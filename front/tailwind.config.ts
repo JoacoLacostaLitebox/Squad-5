32f622f21413b333fbef01898e0d23ddcf64aa44
Laureano Lopez <llopez@litebox.ai>
2024-05-10 19:01:29 -0300

    Global UI updated

--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -8,11 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        'fukuro-blue': '#0000FE',
+        'fukuro-orange': '#FF5A00',
+        'fukuro-white': '#F9F6ED',
+        'fukuro-black': '#282828',
+        'fukuro-bone': '#F5EDCC',
+      }
     },
   },
   plugins: [],
