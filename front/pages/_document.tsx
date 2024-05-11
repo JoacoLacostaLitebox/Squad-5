7f203d44a6843c109703f00c16e9dc23b5fe7ec1
Joaquin Lacosta <jlacosta@litebox.ai>
2024-05-11 04:27:20 -0300

    Fix remove child react

--- front/pages/_document.tsx
+++ front/pages/_document.tsx
@@ -3,9 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
       <Head> <title>Fukuro</title> </Head>
+      <body>
+        <Main />
         <NextScript />
       </body>
     </Html>
