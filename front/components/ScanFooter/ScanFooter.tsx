6bcbe7a6633a2fde70bd4e100b303dfc1f1c0430
Alexandra Gomez <agomez@litebox.ai>
2024-05-11 04:55:03 -0300

    Add Error Modal.

--- front/components/ScanFooter/ScanFooter.tsx
+++ front/components/ScanFooter/ScanFooter.tsx
@@ -3,15 +3,16 @@ import Image from 'next/image';
 import ReverseIcon from "@/public/assets/icons/reverse-icon.svg";
 
 interface ScanFooterProps {
+  isDisabled: boolean;
   onCapture: () => void;
   onReverseCamera: () => void;
 };
 
-const ScanFooter = ({ onCapture, onReverseCamera }: ScanFooterProps) => {
+const ScanFooter = ({ isDisabled, onCapture, onReverseCamera }: ScanFooterProps) => {
   return(
     <div className='flex h-40 w-full fixed bottom-0 rounded-tl-2xl rounded-tr-2xl bg-fukuro-full-white items-center justify-around'>
       <span className='w-10 h-10 rounded-full bg-fukuro-black' />
-      <button className='flex items-center justify-center' onClick={onCapture}>
+      <button className={`flex items-center justify-center ${isDisabled ? 'opacity-30' : ''}`} onClick={onCapture} disabled={isDisabled}>
         <span className='bg-fukuro-orange w-20 h-20 rounded-full opacity-30' />
         <span className='bg-fukuro-orange w-10 h-10 rounded-full absolute' />
       </button>
--- front/components/ScanScreen/ScanScreen.tsx
+++ front/components/ScanScreen/ScanScreen.tsx
@@ -1,6 +1,7 @@
 import ScanFooter from "@/components/ScanFooter/ScanFooter";
 import React, { useRef, useState } from "react";
 import Webcam from "react-webcam";
+import ModalNonRecyclable from "./ModalNonRecyclable";
 
 enum CameraOptions {
   FRONT = 'front',
@@ -9,11 +10,18 @@ enum CameraOptions {
 
 const facingModeDefault = { exact: "environment" };
 
-const ScanScreen: React.FC = () => {
+interface ScanScreenProps {
+  onSuccess: (items: string[]) => void;
+}
+
+const ScanScreen = ({ onSuccess }: ScanScreenProps) => {
   const webcamRef = useRef<any>(null);
   const [selectedCamera, setSelectedCamera] = useState(CameraOptions.BACK);
+  const [showError, setShowError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const onCapture = async () => {
+    setIsLoading(true);
     const imageSrc = webcamRef.current.getScreenshot();
     if (imageSrc) {
       // TODO - Change API URL
@@ -23,33 +31,46 @@ const ScanScreen: React.FC = () => {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({ image: imageSrc }),
-      }).then((res) => {
-        console.log(res);
+      }).then(res => res.json())
+        .then(response => {
+          if (response.length > 0) {
+            onSuccess(response);
+          } else {
+            setShowError(true);
+          }
+        console.log(response);
+      }).finally(() => {
+        setIsLoading(false);
       });
     }
   };
 
   const onClickDifferentCamera = () => {
     setSelectedCamera(prev => prev === CameraOptions.FRONT ? CameraOptions.BACK : CameraOptions.FRONT)
-  };  
+  };
+
+  console.log(isLoading);
 
   return (
-    <div className="flex h-screen flex-col" style={{ background: 'black' }}>
-      <div className="absolute inset-0" style={{ height: 'calc(100vh - 148px)' }}>
-        <Webcam
-          ref={webcamRef}
-          screenshotFormat="image/jpeg"
-          audio={false}
-          videoConstraints={{
-            aspectRatio: 3 / 4,
-            facingMode: selectedCamera === CameraOptions.FRONT
-              ? 'user'
-              : facingModeDefault
-          }}
-          className="absolute inset-0 w-full h-full object-cover"
-        />
+    <div>
+      <div className="flex h-screen flex-col" style={{ background: 'black' }}>
+        <div className="absolute inset-0" style={{ height: 'calc(100vh - 148px)' }}>
+          <Webcam
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            audio={false}
+            videoConstraints={{
+              aspectRatio: 3 / 4,
+              facingMode: selectedCamera === CameraOptions.FRONT
+                ? 'user'
+                : facingModeDefault
+            }}
+            className="absolute inset-0 w-full h-full object-cover"
+          />
+        </div>
+        <ScanFooter onCapture={onCapture} onReverseCamera={onClickDifferentCamera} isDisabled={isLoading} />
       </div>
-      <ScanFooter onCapture={onCapture} onReverseCamera={onClickDifferentCamera} />
+      {showError && <ModalNonRecyclable onClose={() => setShowError(false)} />}
     </div>
   )
 };
