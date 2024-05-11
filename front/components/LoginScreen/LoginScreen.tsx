d4135552052692c6ea44d564ca3655cc483bec9f
jcalvano <jcalvano@litebox.ai>
2024-05-10 23:02:59 -0300

    working on Login

--- front/components/LoginScreen/LoginScreen.tsx
+++ front/components/LoginScreen/LoginScreen.tsx
@@ -1,19 +1,37 @@
-// Next
-import localFont from "next/font/local";
+// Components
 import Branding from "../Branding/Branding";
 import Footer from "../Footer/Footer";
+import BottomDecoration from "../BottomDecoration/BottomDecoration";
+import Button from "../Button/Button";
+
+// Fonts
+import localFont from "next/font/local";
+
+// Context
+import { useAuth } from "@/context/AuthContext";
 
 // Font
 const Phonk = localFont({ src: "../../public/fonts/PhonkContrast.otf" });
 
+// Assets
+import googleIcon from './assets/googleIcon.svg'
+
 const LoginScreen = () => {
+    const { googleSignIn } = useAuth();
     const welcomeMessages = ["Te damos", "la bienvenida", "al fukuro"];
+
+    const handleSignIn = async () => {
+        try {
+            await googleSignIn();
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     return (
         <div className={`relative flex flex-col h-screen overflow-hidden items-center bg-fukuro-white w-full`}>
             <Branding />
-
-            <div className={`${Phonk.className} justify-center w-full max-w-[251px]`}>
+            <div className={`${Phonk.className} justify-center w-full max-w-[251px] `}>
                 {welcomeMessages.map((message, index) => (
                     <h2 key={index} className={`text-fukuro-black text-[28px] text-center`}>
                         {message}
@@ -22,8 +40,10 @@ const LoginScreen = () => {
             </div>
 
             <div>
-                
+                <Button onClick={() => { handleSignIn() }} text="Ingresar con Google" leftIcon={googleIcon} />
+
             </div>
+            <BottomDecoration />
             <div className={'absolute inset-x-0 bottom-[80px] h-16'} >
                 <Footer />
             </div>
