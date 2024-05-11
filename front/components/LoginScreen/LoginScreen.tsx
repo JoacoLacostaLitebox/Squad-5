d6a2393a30936ee8cd0b70a82fa8b3eac474bc10
Laureano Lopez <llopez@litebox.ai>
2024-05-10 22:36:08 -0300

    Homepage updated

--- front/components/LoginScreen/LoginScreen.tsx
+++ front/components/LoginScreen/LoginScreen.tsx
@@ -5,17 +5,16 @@ import Branding from "../Branding/Branding";
 // Font
 const Phonk = localFont({ src: "../../public/fonts/PhonkContrast.otf" });
 
-const LoginScreen = ({onClick}: {onClick: () => void}) => {
-
-    return(
-        <div className={`relative flex flex-col h-screen w-screen overflow-hidden items-center bg-fukuro-white`}>
-            <Branding/>
-
-            <div className={`flex flex-col w-screen bg-fukuro-black`}>
-                <button onClick={onClick}>Login</button>
-            </div>
-
-        </div>
-    )
-}
+const LoginScreen = ({ onClick }: { onClick: () => void }) => {
+  return (
+    <div
+      className={`relative flex flex-col h-screen w-screen overflow-hidden items-center bg-fukuro-white`}
+    >
+      <Branding />
+      <div className={`flex flex-col w-screen bg-fukuro-black`}>
+        <button onClick={onClick}>Login</button>
+      </div>
+    </div>
+  );
+};
 export default LoginScreen;
--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -2,6 +2,7 @@
 
 // Next
 import localFont from "next/font/local";
+import { DM_Sans } from "next/font/google";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -11,12 +12,16 @@ import { useAuth } from "@/context/AuthContext";
 import Footer from "@/components/Footer/Footer";
 import PointsContainer from "@/components/PointsContainer/PointsContainer";
 import Button from "@/components/Button/Button";
-import ArrowRight from "@/public/assets/arrow-right.svg";
+import LogoutIcon from "@/public/assets/icons/log-out.svg";
 import BottomDecoration from "@/components/BottomDecoration/BottomDecoration";
 import LoginScreen from "@/components/LoginScreen/LoginScreen";
+import Branding from "@/components/Branding/Branding";
+import { User } from "firebase/auth";
+import Image from "next/image";
 
 // Font
 const Phonk = localFont({ src: "../public/fonts/PhonkContrast.otf" });
+const DMSans = DM_Sans({ style: ["normal"], subsets: ["latin"] });
 
 export default function Home() {
   const pathname = usePathname();
@@ -40,6 +45,12 @@ export default function Home() {
     }
   };
 
+  const getDisplayName = (user: User) => {
+    if (user && user.displayName)
+      return `¡Hola, ${user.displayName.split(" ")[0]}!`;
+    return "¡Hola!";
+  };
+
   useEffect(() => {
     if (isLoading) {
       return;
@@ -58,12 +69,21 @@ export default function Home() {
       ) : !user ? (
         <LoginScreen onClick={handleSignIn} />
       ) : (
-        <div className="flex flex-col h-screen w-screen bg-fukuro-white justify-between align-center">
-          <p>Welcome, {user.displayName}</p>
-          <p className="cursor-pointer" onClick={handleSignOut}>
-            Sign out
-          </p>
-          <PointsContainer points={374930} />
+        <div className="flex flex-col h-screen w-screen bg-fukuro-white justify-between">
+          <div className="flex flex-col h-full w-full relative px-6">
+            <div className="flex relative justify-center items-center w-full h-fit mt-8">
+              <Branding />
+              <button onClick={handleSignOut} className="absolute right-0">
+                <Image src={LogoutIcon} alt="log out" />
+              </button>
+            </div>
+            <p
+              className={`${DMSans.className} text-fukuro-black font-bold text-2xl mt-6`}
+            >
+              {getDisplayName(user)}
+            </p>
+            <PointsContainer points={374930} />
+          </div>
           <Footer />
           <BottomDecoration />
         </div>
