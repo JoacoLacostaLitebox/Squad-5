6a35f9c58fd9fd64c8252ad7b0ff05adc897180f
jcalvano <jcalvano@litebox.ai>
2024-05-11 04:21:06 -0300

    Fix: Metadata

--- front/pages/_document.tsx
+++ front/pages/_document.tsx
@@ -1,17 +1,11 @@
-import { Metadata } from "next";
 import { Html, Head, Main, NextScript } from "next/document";
-
-export const metadata: Metadata = {
-  title: "Fukuro",
-  description: "Fukuro App",
-};
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <Main />
+      <Head> <title>Fukuro</title> </Head>
         <NextScript />
       </body>
     </Html>
